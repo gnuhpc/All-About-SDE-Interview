ba20ca329387dd3d31d9ece7deb1ad3dab4c9b05
gnuhpc <gnuhpc@gmail.com>
2019-01-31 16:19:14 +0800

    Add two design pattern and two leetcode problems.

---
+++ src/main/java/org/gnuhpc/bigdata/leetcode/CopyRandomList138.java
@@ -0,0 +1,45 @@
+package org.gnuhpc.bigdata.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int x) { this.label = x; }
+};
+
+public class CopyRandomList138 {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode pre = dummy, newNode;
+        while (head != null) {
+            if (map.containsKey(head)) {
+                newNode = map.get(head);
+            } else {
+                newNode = new RandomListNode(head.label);
+                map.put(head, newNode);
+            }
+            pre.next = newNode;
+
+            if (head.random != null) {
+                if (map.containsKey(head.random)) {
+                    newNode.random = map.get(head.random);
+                } else {
+                    newNode.random = new RandomListNode(head.random.label);
+                    map.put(head.random, newNode.random);
+                }
+            }
+
+            pre = newNode;
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}

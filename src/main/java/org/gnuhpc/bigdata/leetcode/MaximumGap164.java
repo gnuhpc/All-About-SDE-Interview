3bf39fdb4cd42e99d845fbed97ee76f298c77eea
gnuhpc <gnuhpc@gmail.com>
2019-07-29 17:10:40 +0800

    commit

--- src/main/java/org/gnuhpc/bigdata/leetcode/MaximumGap164.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/MaximumGap164.java
@@ -5,93 +5,49 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class MaximumGap164 {
-    //Brute force, O(nlogn)
+    //Method 1: using bucket sort like method
     public int maximumGap(int[] nums) {
-        if (nums == null || nums.length<2) return 0;
-
-        Arrays.sort(nums);
-
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - nums[i-1] > max){
-                max = nums[i] - nums[i-1];
-            }
-        }
-
-        return max;
-    }
-
-
-    //Method 2: using bucket sort like method
-    class Bucket{
-        int low;
-        int high;
-        public Bucket(){
-            low = -1;
-            high = -1;
-        }
-    }
-
-    public int maximumGap2(int[] num) {
-        if(num == null || num.length < 2){
+        if (nums == null || nums.length < 2) {
             return 0;
         }
-
-        int max = num[0];
-        int min = num[0];
-        for(int i=1; i<num.length; i++){
-            max = Math.max(max, num[i]);
-            min = Math.min(min, num[i]);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
         }
-
-        // initialize an array of buckets
-        //映射桶的第一步：创建一个length+1的数组
-        Bucket[] buckets = new Bucket[num.length+1]; //project to (0 - n)
-        for(int i=0; i<buckets.length; i++){
-            buckets[i] = new Bucket();
+        if (min == max) {
+            return 0;
         }
-
-        //映射桶的第二步，获取interval
-        double interval = interval(num.length,max,min);
-        //distribute every number to a bucket array
-        for(int i=0; i<num.length; i++){
-            //映射桶的第三步，获取映射的index
-            int index = index(num[i], min, interval);
-
-            if(buckets[index].low == -1){
-                buckets[index].low = num[i];
-                buckets[index].high = num[i];
-            }else{
-                buckets[index].low = Math.min(buckets[index].low, num[i]);
-                buckets[index].high = Math.max(buckets[index].high, num[i]);
+        boolean[] hasNum = new boolean[len + 1];
+        int[] mins = new int[len + 1];
+        int[] maxs = new int[len + 1];
+        int bid = 0;
+        for (int i = 0; i < len; i++) {
+            bid = bucket(nums[i], len, min, max);
+            mins[bid] = hasNum[bid] ? Math.min(mins[bid], nums[i]) : nums[i];
+            maxs[bid] = hasNum[bid] ? Math.max(maxs[bid], nums[i]) : nums[i];
+            hasNum[bid] = true;
+        }
+        int res = 0;
+        int lastMax = maxs[0];
+        int i = 1;
+        for (; i <= len; i++) {
+            if (hasNum[i]) {
+                res = Math.max(res, mins[i] - lastMax);
+                lastMax = maxs[i];
             }
         }
-
-        //scan buckets to find maximum gap, 因为落在同一个桶内的数字大小差最大为ceil(interval)
-        int result = 0;
-        int prev = buckets[0].high;
-        for(int i=1; i<buckets.length; i++){
-            if(buckets[i].low != -1){
-                result = Math.max(result, buckets[i].low-prev);
-                prev = buckets[i].high;
-            }
-
-        }
-
-        return result;
+        return res;
     }
 
-    private double interval(int length, int max, int min){
-        return (double) length / (max - min); //一格距离代表的原数组长度
+    private int bucket(long num, long len, long min, long max) {
+        return (int) ((num - min) * len / (max - min));
     }
 
-    private int index(int num, int min, double interval){
-        return (int) ((num - min) * interval);
-    }
-
-    // Method 3 标准bucket，可能会内存不够用
-    public int maximumGap3(int[] nums) {
+    // Method 2 标准bucket，内存不够用
+    public int maximumGap2(int[] nums) {
         if(nums == null || nums.length < 2){
             return 0;
         }

199dd7fbd6c6deb8f96021d6d04082cedc7702c0
tina437213 <tina437213@163.com>
2019-09-22 20:15:36 +0800

    commit by tina

--- src/main/java/org/gnuhpc/bigdata/leetcode/MaxSlidingWindow239.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/MaxSlidingWindow239.java
@@ -99,6 +99,76 @@ sliding_max = 4, 6, 6, 8, 9, 10, 12, 56
         return sliding_max;
     }
 
+    // add by tina
+    // 借助双端队列
+    /**
+     We scan the array from 0 to n-1, keep "promising" elements in the deque. The algorithm is amortized O(n) as each element is put and polled once.
+     At each i, we keep "promising" elements, which are potentially max number in window [i-(k-1),i] or any subsequent window. This means
+     1、If an element in the deque and it is out of i-(k-1), we discard them. We just need to poll from the head, as we are using a deque and elements are ordered as the sequence in the array
+     2、Now only those elements within [i-(k-1),i] are in the deque. We then discard elements smaller than a[i] from the tail. This is because if a[x] <a[i] and x<i, then a[x] has no chance to be the "max" in [i-(k-1),i], or any other subsequent window: a[i] would always be a better candidate.
+     3、As a result elements in the deque are ordered in both sequence in array and their value. At each step the head of the deque is the max element in [i-(k-1),i]
+     */
+    public int[] maxSlidingWindow4(int[] nums, int k) {
+        if(nums==null||nums.length==0)
+            return new int[0];
+
+        int[] result = new int[nums.length-k+1];
+        // store index，Linkedlist实现了Deque接口
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for(int i=0; i<nums.length; i++){
+            // 维持deque最大包含k个数，remove numbers out of range k
+            if(!deque.isEmpty()&&deque.peekFirst()==i-k)
+                deque.poll();
+            // 维持deque是降序的，remove smaller numbers in k range as they are useless
+            while(!deque.isEmpty()&&nums[deque.peekLast()]<nums[i]){
+                deque.removeLast();
+            }
+            deque.offer(i);
+            // 从形成第一个窗口开始可以往结果数组写数
+            if(i>=k-1)
+                result[i+1-k]=nums[deque.peek()];
+        }
+        return result;
+    }
+
+    // add by tina
+    // 优化暴力解，考虑当前窗口【low,high】，维护窗口最大值与位置
+    // 如果上一个窗口的最大值位置为low，那么本窗口这个最大值会被移出去，
+    // 意味着本窗口需重新寻找最大值
+    // 如果不是，那么比较新加入的值，与窗口最大值即可。
+    public int[] maxSlidingWindow5(int[] nums, int k) {
+        if(nums == null ) return null;
+        if(nums.length == 0 || nums.length == 1) return nums;
+        int len = nums.length;
+        int[] res = new int[len-k+1];
+        int maxPos = -1;
+        int low = 0;
+        int high = k-1;
+        int max = nums[0];
+        while(high < len){
+            if(low-1 == maxPos) {
+                max = nums[low];
+                maxPos = low;
+                for(int i = low+1;i<=high;i++){
+                    if(nums[i] > max){
+                        max = nums[i];
+                        maxPos = i;
+                    }
+                }
+
+            }else if(nums[high] >= max){
+                max = nums[high];
+                maxPos = high;
+            }
+            res[low]=max;
+            low++;
+            high++;
+        }
+        return res;
+
+    }
+
+
     @Test
     public void test() {
         Utils.printArray(maxSlidingWindow2(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));

89f8afdc07fdea0c8e729571d3fcf1a1174f11dd
tina437213 <tina437213@163.com>
2019-09-30 11:42:14 +0800

    commit by tina,20190928

--- src/main/java/org/gnuhpc/bigdata/leetcode/Rob198.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/Rob198.java
@@ -58,4 +58,96 @@ public class Rob198 {
 
         return res[nums.length-1];
     }
+
+    //add by tina
+    /**
+     * 1、没有思路，先想暴力解法。本质是从数组里面取x（x=[0,n-1]）个数组合问题，
+     * 然后遍历组合，求最优解。O((2^n)*n)。通常求最优解，可以尝试找找递归+重叠字问题
+     * 2、memo search的状态定义是考虑偷取[x,...,n-1]范围里的房子
+     * 状态转移方程是 f(0) = max{v(0)+f(2),v(1)+f(3),v(2)+f(4),...,v(n-3)+f(n-1),v(n-2),v(n-1)}
+     * 3、DP参考上面的状态方程
+     * 4、考虑memo search另一套状态定义，考虑偷取[0,...,x]范围的房子
+     * 状态转移方程是f(n-1) = max{v(n-1)+f(n-3),v(n-2)+f(n-4),...,v(2)+f(0),v(1),v(0)}
+     */
+
+    //private int[] memo;
+    public int rob3(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        //因为数组是非负整数，递归遍历中可能取单个元素的组合，故需要初始化为-1，而不是0
+        //也可以定义为Integer[] memo，做判空处理
+        memo = new int[nums.length];
+        Arrays.fill(memo,-1);
+        return tryRob(nums,0);
+
+    }
+    //考虑偷取[x,...,n-1]范围里的房子
+    public int tryRob(int[] nums,int index){
+        if(index >= nums.length) return 0;
+        if(memo[index] != -1) return memo[index];
+        int mx = 0;
+        for(int i = index;i<nums.length;i++){//注意i从index开始
+            mx = Math.max(mx,nums[i] + tryRob(nums,i+2));
+        }
+        memo[index] = mx;
+        return mx;
+    }
+
+    public int rob4(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        //因为数组是非负整数，递归遍历中可能取单个元素的组合，故需要初始化为-1，而不是0
+        // DP双重循环,考虑偷取[x,...,n-1]范围里的房子
+        // 初始状态DP[n-1] = nums[n-1];DP[n-2] = max(nums[n-2],DP[n-1])
+        // DP[i] = max{nums[i]+dp[i+2],nums[i+1]+dp[i+3],...,nums[n-1]}
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[n-1] = nums[n-1];
+        for(int i = n-2; i>=0;i--)
+            for(int j = i;j<n;j++){
+                //因为要用dp[j+2]，需对j+2做合法判断
+                dp[i] = Math.max(dp[i],nums[j]+(j+2<n ? dp[j+2]:0));
+            }
+        return dp[0];
+    }
+
+    // memo search
+    // f(n-1) = max{v(n-1)+f(n-3),v(n-2)+f(n-4),...,v(2)+f(0),v(1),v(0)}
+    public int rob5(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        //因为数组是非负整数，递归遍历中可能取单个元素的组合，故需要初始化为-1，而不是0
+        //也可以定义为Integer[] memo，做判空处理
+        memo = new int[nums.length];
+        Arrays.fill(memo,-1);
+        return tryRob(nums,nums.length-1);
+
+    }
+    //考虑偷取[0,...,x]范围里的房子
+    public int tryRob5(int[] nums,int index){
+        if(index <0) return 0;
+        if(memo[index] != -1) return memo[index];
+        int mx = 0;
+        for(int i = index;i>=0;i++){//注意i从index开始
+            mx = Math.max(mx,nums[index] + tryRob(nums,i-2));
+        }
+        memo[index] = mx;
+        return mx;
+    }
+
+    // DP[i] = max{nums[i]+DP[i-2],nums[i-1]+DP[i-3],...,nums[2]+DP[0],nums[1],nums[0])
+    public int rob6(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        //因为数组是非负整数，递归遍历中可能取单个元素的组合，故需要初始化为-1，而不是0
+        // DP双重循环,考虑偷取[x,...,n-1]范围里的房子
+        // 初始状态DP[0] = nums[0];DP[1] = max(nums[1],DP[0])
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i = 0; i<n;i++)
+            for(int j = i;j>=0;j--){
+                //因为要用dp[j+2]，需对j+2做合法判断
+                dp[i] = Math.max(dp[i],nums[j]+(j-2>=0 ? dp[j-2]:0));
+            }
+        return dp[n-1];
+    }
+
 }
--- src/main/java/org/gnuhpc/bigdata/leetcode/UniquePathsWithObstacles63.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/UniquePathsWithObstacles63.java
@@ -138,7 +138,7 @@ public class UniquePathsWithObstacles63 {
 
     }
 
-    // add by tina,TODO
+    // add by tina,TODO，对比62，仅是DP的条件有变化
     public int uniquePathsWithObstacles5(int[][] obstacleGrid) {
         if(obstacleGrid == null || obstacleGrid[0] == null) return 0;
         int m = obstacleGrid.length;
@@ -151,7 +151,7 @@ public class UniquePathsWithObstacles63 {
             for(int j=0;j<n;j++){//以下这些条件是有先后顺序的
                 if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
                 else if(i==0&&j==0) dp[i][j] = 1;
-                else if(i==0 && j>0) dp[i][j] = dp[i][j-1];//第一行
+                else if(i==0 && j>0) dp[i][j] = dp[i][j-1];//第一行，保持和DP[0][0]一致
                 else if(j==0 && i>0) dp[i][j] = dp[i-1][j];//第一列
                 else dp[i][j] = dp[i-1][j] + dp[i][j-1] ;
             }

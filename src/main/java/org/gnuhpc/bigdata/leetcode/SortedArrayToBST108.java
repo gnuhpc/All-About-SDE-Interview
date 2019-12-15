92175cf3affbf0a9e92ebc80349649dd8366d3d2
gnuhpc <gnuhpc@gmail.com>
2019-12-15 15:17:37 +0800

    commit by gnuhpc

--- src/main/java/org/gnuhpc/bigdata/leetcode/SortedArrayToBST108.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/SortedArrayToBST108.java
@@ -26,46 +26,4 @@ public class SortedArrayToBST108 {
         return node;
     }
 
-    /*
-    Method2 : dfs
-     */
-
-    private static class MyNode {
-        TreeNode node;
-        int leftBound, rightBound;
-        public MyNode(TreeNode node, int left, int right) {
-            this.node = node;
-            this.leftBound = left;
-            this.rightBound = right;
-        }
-    }
-
-    // Iterative version
-    public static TreeNode sortedArrayToBST_Iterative_DFS(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return null;
-
-        TreeNode root = new TreeNode(0); // We start with zero as default
-        Deque<MyNode> stack = new LinkedList<>();
-
-        MyNode node = new MyNode(root, 0, nums.length - 1);
-        stack.push(node);
-
-        while (!stack.isEmpty()) {
-            MyNode cur = stack.pop();
-            int mid = cur.leftBound + (cur.rightBound - cur.leftBound) / 2;
-            cur.node.val = nums[mid];
-
-            if (cur.leftBound < mid) {
-                cur.node.left = new TreeNode(0);
-                stack.push(new MyNode(cur.node.left, cur.leftBound, mid - 1));
-            }
-
-            if (cur.rightBound > mid) {
-                cur.node.right = new TreeNode(0);
-                stack.push(new MyNode(cur.node.right, mid + 1, cur.rightBound));
-            }
-        }
-        return root;
-    }
 }
--- src/main/java/org/gnuhpc/bigdata/misc/Solution.java
+++ src/main/java/org/gnuhpc/bigdata/misc/Solution.java
@@ -1,12 +1,56 @@
 package org.gnuhpc.bigdata.misc;
 
+import org.junit.Test;
+
 import java.util.*;
 
 /**
  * Copyright gnuhpc 2019/11/5
  */
 public class Solution {
-    public static void main(String[] args) {
-        
+    List<Integer>       res         = new ArrayList<>();
+    Map<Integer, Float> idRatingMap = new HashMap<>();
+
+    @Test
+    public void test() {
+
+    }
+
+    public List<Integer> filterAndSortBusinesses(
+            List<Business> businesses,
+            boolean onlyVeganFriendly,
+            Integer maxPrice,
+            Float maxDistance
+    ) {
+        // Todo: Complete Me
+
+        for (Business b : businesses) {
+            idRatingMap.put(b.id, b.rating);
+            if (b.veganFriendly != onlyVeganFriendly) {
+                continue;
+            }
+
+            if (maxPrice != null) {
+                if (b.price > maxPrice) continue;
+            }
+
+            if (maxDistance != null) {
+                if (b.distance > maxDistance) continue;
+            }
+
+            res.add(b.id);
+        }
+
+        Collections.sort(res, (a, b) -> Float.compare(idRatingMap.get(a), idRatingMap.get(b)));
+
+        return res;
+    }
+
+    private class Business {
+        public Integer id;
+        public Float   rating;
+        public Boolean veganFriendly;
+        public Integer price;
+        public Float   distance;
     }
 }

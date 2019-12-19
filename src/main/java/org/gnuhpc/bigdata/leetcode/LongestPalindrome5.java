88eaa0a67b06dbd06d00c8244aba79bbef78bd79
gnuhpc <gnuhpc@gmail.com>
2019-12-19 14:02:06 +0800

    commit by gnuhpc

--- src/main/java/org/gnuhpc/bigdata/leetcode/LongestPalindrome5.java
+++ src/main/java/org/gnuhpc/bigdata/leetcode/LongestPalindrome5.java
@@ -2,11 +2,12 @@ package org.gnuhpc.bigdata.leetcode;
 
 import org.junit.Test;
 
+//DONE
 public class LongestPalindrome5 {
     @Test
     public void test(){
-        String s = "babad";
-        System.out.println(longestPalindrome(s));
+        String s = "a";
+        System.out.println(longestPalindrome2(s));
     }
 
 
@@ -46,4 +47,35 @@ public class LongestPalindrome5 {
         }
         return s.substring(l + 1, r); // note the range is from l + 1 to r - 1  都是闭区间
     }
+
+    /*
+    Method2 : DP
+    ## Time complexity
+    `O(n^2)` since we have to fill the 2D array.
+
+    ## Space complexity
+    `O(n^2)` since we store information in the 2D array.
+     */
+    public String longestPalindrome2(String s) {
+        if (s == null || s.length() == 0) return "";
+        String ret = "";
+        char[] sCh = s.toCharArray();
+        int len = s.length();
+        boolean[][] dp = new boolean[len + 1][len + 1];
+        for (int i = len - 1; i >= 0; i--) {//为什么从后往前是因为递归公式中需要i+1的结果
+            for (int j = i; j < len; j++) {
+                if(i == j) dp[i][j] = true; //base case 1
+                else if(j == i+1) dp[i][j] = (sCh[i] == sCh[j]);//base case 2
+                else //递推公式
+                    dp[i][j] = (sCh[i] == sCh[j])? dp[i + 1][j - 1]:false;
+
+                if (dp[i][j] && (j - i + 1 > ret.length())) {//获取最长的回文子串
+                    ret = s.substring(i, j + 1);
+                }
+            }
+        }
+        return ret;
+    }
+
+
 }

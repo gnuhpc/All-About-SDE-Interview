2401ccead9b875bc8020fb001f4ee1387d01c4db
gnuhpc <gnuhpc@gmail.com>
2020-02-12 11:53:56 +0800

    commit by gnuhpc

---
+++ src/main/java/org/gnuhpc/bigdata/leetcode/RandomPickIndex398.java
@@ -0,0 +1,28 @@
+package org.gnuhpc.bigdata.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Copyright gnuhpc 2020/2/12
+ */
+public class RandomPickIndex398 {
+    private int[] nums;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int pick(int target) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                list.add(i);
+            }
+        }
+        Random rand = new Random();
+        //假设list.size() = 10; 则 rand.next(list.size()) 生成 [0, 10)
+        return list.get(rand.nextInt(list.size()));
+    }
+}

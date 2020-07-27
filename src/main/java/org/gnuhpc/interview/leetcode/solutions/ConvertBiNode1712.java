ba2796d52dc8aebea8b7c2a0d62ecd1de1a10787
gnuhpc <futuredaemon@gmail.com>
2020-07-27 10:52:58 +0800

    daily commit

---
+++ src/main/java/org/gnuhpc/interview/leetcode/solutions/ConvertBiNode1712.java
@@ -0,0 +1,17 @@
+package org.gnuhpc.interview.leetcode.solutions;
+
+import org.gnuhpc.interview.leetcode.utils.TreeNode;
+
+public class ConvertBiNode1712 {
+    TreeNode head=new TreeNode(0),pre=head;
+    public TreeNode convertBiNode(TreeNode root) {
+        if(root==null)
+            return null;
+        convertBiNode(root.left);
+        pre.right=root;
+        root.left=null;
+        pre=root;
+        convertBiNode(root.right);
+        return head.right;
+    }
+}
